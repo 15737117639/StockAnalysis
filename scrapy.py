56afd19271147a1620da2f2ad8654505a781924d
SuiMingYang <2282420654@qq.com>
2020-07-01 17:03:16 +0800

    多线程

--- scrapy.py
+++ scrapy.py
@@ -20,57 +20,86 @@ import datetime
 import os
 import time
 import shutil
+import threading
+from threadpool import ThreadPool,makeRequests
 from Config.base import conf
 import token
 
-def program():
+class Stock(object):
+    def __init__(self):
+        self.lock = threading.Lock()
 
-    if os.path.exists(conf.get('file','name')):
-        ts_name_code=pd.read_csv(conf.get('file','name'))
-    else:
-        ts_name_code=token.get_namecode()
+    def __get_namecode__(self):
+        if os.path.exists(conf.get('file','name')):
+            ts_name_code=pd.read_csv(conf.get('file','name'))
+        else:
+            ts_name_code=token.get_namecode()
 
-    ts_code = ts_name_code['ts_code']
-    name_code=ts_name_code['name']
-    industry=ts_name_code['industry']
-    area=ts_name_code['area']
-    start=datetime.datetime.now()
-    priord=datetime.timedelta(days=int(conf.get('var','timerank')))
+        self.ts_code = ts_name_code['ts_code']
+        self.name_code=ts_name_code['name']
+        self.industry=ts_name_code['industry']
+        self.area=ts_name_code['area']
+        start=datetime.datetime.now()
+        self.priord=datetime.timedelta(days=int(conf.get('var','timerank')))
+        self.__get_timerank__(start)
 
-    timerank=[]
-    for i in range(int(conf.get('var','for_time'))):
-        t=[]
-        st=start-datetime.timedelta(days=int(conf.get('var','backday')))
-        et=st-priord
-        t.append(et.strftime(conf.get('var','format')))
-        t.append(st.strftime(conf.get('var','format')))
-        start=et
-        timerank.append(t)
+    def __get_timerank__(self,start):
+        self.timerank=[]
+        for i in range(int(conf.get('var','for_time'))):
+            t=[]
+            st=start-datetime.timedelta(days=int(conf.get('var','backday')))
+            et=st-self.priord
+            t.append(et.strftime(conf.get('var','format')))
+            t.append(st.strftime(conf.get('var','format')))
+            start=et
+            self.timerank.append(t)
 
-    timerank=timerank[::-1]
+        #self.timerank=timerank[::-1]
 
-    if os.path.exists(conf.get('dir','stock')):
-        shutil.rmtree(conf.get('dir','stock'))
-        os.mkdir(conf.get('dir','stock'))
-    else:
-        os.mkdir(conf.get('dir','stock'))
+    def program(self):
+        self.__get_namecode__()
 
-    for i,code in enumerate(ts_code):
-        for [st,et] in timerank:
-            res=req.get(conf.get('config','req_url').format(code,st,et,'ts_code'))
+        if os.path.exists(conf.get('dir','stock')):
+            shutil.rmtree(conf.get('dir','stock'))
+            os.mkdir(conf.get('dir','stock'))
+        else:
+            os.mkdir(conf.get('dir','stock'))
 
-            print(len(res.json()['list']))
-            with open(conf.get('file','json'),"w",encoding="utf-8") as f:
-                f.write(json.dumps(res.json()['list']))
+        pool = ThreadPool(10)
+        param=[]
+        for i,code in enumerate(self.ts_code):
+            for [st,et] in self.timerank:
+                url=conf.get('config','req_url').format(code,st,et,'ts_code')
+                param.append(([i,code,url,st,et],None))
+        
+        reqs = makeRequests(self.get_data,param)
 
-            data = pd.read_json(conf.get('file','json'),encoding="utf-8", orient='records')
-            data = data.drop_duplicates(['id','ts_code','trade_date'])
-            filename=conf.get('dir','stock')+'{}_{}_{}.csv'.format(code,name_code[i],industry[i])
-            if os.path.exists(filename):
-                data.to_csv(filename,header=None,mode='a',index=None)
-            else:
-                data.to_csv(filename,index=None)
-            #time.sleep(1)
+        [pool.putRequest(req) for req in reqs] 
+
+        pool.wait()
+
+    def get_data(self,i,code,url,st,et):
+        #for i,code in enumerate(ts_code):
+        res=req.get(url)
+        print(code,self.name_code[i],self.industry[i],len(res.json()['list']))
+        jsonfile=conf.get('dir','stock')+'{}_{}_{}_{}_{}.json'.format(code,self.name_code[i],self.industry[i],st,et)
+
+        with open(jsonfile,"w",encoding="utf-8") as f:
+            f.write(json.dumps(res.json()['list']))
+
+        filename=conf.get('dir','stock')+'{}_{}_{}.csv'.format(code,self.name_code[i],self.industry[i])
+
+        data = pd.read_json(jsonfile,encoding="utf-8", orient='records')
+        data = data.drop_duplicates(['id','ts_code','trade_date'])
+        self.lock.acquire(1)
+        if os.path.exists(filename):
+            data.to_csv(filename,header=None,mode='a',index=None)
+        else:
+            data.to_csv(filename,index=None)
+        self.lock.release()
+        #time.sleep(1)
+        os.remove(jsonfile)
 
 if __name__ == "__main__":
-    program()
+    stock=Stock()
+    stock.program()
